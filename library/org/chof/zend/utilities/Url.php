b817df37e9258790bf0b03810537ef1b7974dc06
Christian Hofbauer <chof@gmx.at>
2013-08-27 06:30:49 +0000

    Added getClientIP Method
    git-svn-id: svn://test.cloud/trunk/zend-framework@679 d188707f-1a3f-ee44-8807-bfccb9f3d39a

--- library/org/chof/zend/utilities/Url.php
+++ library/org/chof/zend/utilities/Url.php
@@ -1,29 +1,45 @@
 <?php
 
-class Chof_Util_Url 
+class Chof_Util_Url
 {
-  public static function url($controller, $action, $params, $base = '')
-  #*****************************************************************************
-  {
-    $p = array();
-    foreach($params as $param => $value)
-    {
-      $p []= "$param=$value";
-    }
-       
-    $url  = join('/', array($base, $controller, $action));
-    $url .= (count($p)>0) ? '?'.join('&', $p) : '';
-    
-    return $url;
-    
-  }
-  
-  public static function baseUrl($url)
-  #*****************************************************************************
-  {
-    $controller = Zend_Controller_Front::getInstance();
-    return $controller->getBaseUrl().'/'.$url;
-  }
-}
+	public static function url($controller, $action, $params, $base = '')
+	#*****************************************************************************
+	{
+		$p = array();
+		foreach($params as $param => $value)
+		{
+			$p []= "$param=$value";
+		}
+		 
+		$url  = join('/', array($base, $controller, $action));
+		$url .= (count($p)>0) ? '?'.join('&', $p) : '';
 
-?>
+		return $url;
+
+	}
+
+	public static function baseUrl($url)
+	#*****************************************************************************
+	{
+		$controller = Zend_Controller_Front::getInstance();
+		return $controller->getBaseUrl().'/'.$url;
+	}
+
+	public static function getClientIP()
+	#*****************************************************************************
+	{
+		if ( isset($_SERVER["REMOTE_ADDR"]) ) 
+		{
+			return $_SERVER["REMOTE_ADDR"];
+		} 
+		else if ( isset($_SERVER["HTTP_X_FORWARDED_FOR"]) )    
+		{
+			return $_SERVER["HTTP_X_FORWARDED_FOR"];
+		} 
+		else if ( isset($_SERVER["HTTP_CLIENT_IP"]) )    
+		{
+			return $_SERVER["HTTP_CLIENT_IP"];
+		}
+	}
+}
+	?>
