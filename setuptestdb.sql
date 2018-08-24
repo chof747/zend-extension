c75069b04db6a5333a5bf0c7fee41eede2ddae76
Christian Hofbauer <chof@gmx.at>
2018-08-24 13:00:31 +0200

    sql correction

--- setuptestdb.sql
+++ setuptestdb.sql
@@ -1,6 +1,6 @@
 DROP DATABASE IF EXISTS zendtest;
 CREATE DATABASE zendtest;
 
-DROP USER IF EXISTS `zend-user`@`localhost`;
+DROP USER `zend-user`@`localhost`;
 CREATE USER `zend-user`@`localhost` IDENTIFIED BY "zend";
 GRANT SELECT, UPDATE, DELETE, EXECUTE ON `zendtest`.* TO `zend-user`@`localhost`;
