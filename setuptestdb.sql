37177270c2f5bb27d18555cddfd7dc9407b81001
chof747 <chof@gmx.at>
2018-10-03 23:34:51 +0200

    another try with removed drop user

--- setuptestdb.sql
+++ setuptestdb.sql
@@ -1,7 +1,6 @@
 DROP DATABASE IF EXISTS zendtest;
 CREATE DATABASE zendtest;
-
---DROP USER 'zend-user'@'localhost';
+-- DROP USER 'zend-user'@'localhost';
 CREATE USER `zend-user`@`localhost` IDENTIFIED BY "zend";
 GRANT SELECT, UPDATE, DELETE, EXECUTE ON `zendtest`.* TO `zend-user`@`localhost`;
 
